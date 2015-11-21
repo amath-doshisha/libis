57c43bde3d5a3d667e895dc73c8a5c032083e50c
kon <kokondo@mail.doshisha.ac.jp>
2015-11-20 22:50:34 -0500

    Add matlab/TEST_erros_*.m

---
+++ matlab/TEST_errors_double.m
@@ -0,0 +1,7 @@
+clear all
+close all
+
+x=665857
+y=470832
+z=x^4-4*y^2-4*y^4
+% exact: z=1
---
+++ matlab/TEST_errors_multi.m
@@ -0,0 +1,9 @@
+clear all
+close all
+
+set_default_prec(128)
+
+x=multi(665857)
+y=multi(470832)
+z=x^4-4*y^2-4*y^4
+% exact: z=1
