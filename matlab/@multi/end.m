2ee0e0ebc82279ee5c5c991e57908fd47d0991a2
kon <kokondo@mail.doshisha.ac.jp>
2015-11-21 20:53:24 -0500

    20151121b. Modify matlab/end.m

--- matlab/@multi/end.m
+++ matlab/@multi/end.m
@@ -1,8 +1,11 @@
 %% ind=end(A,k,n)
-function ind=end(obj,k,n)
-szd=size(obj.data);
-if k<n
-    ind=szd(k);
+function ind=end(A,k,n)
+if n==1
+    ind=numel(A);
+elseif k<n
+    s=size(A.data);
+    ind=s(k);
 else
-    ind=prod(szd(k:n));
+    s=size(A.data);
+    ind=prod(s(k:n));
 end
