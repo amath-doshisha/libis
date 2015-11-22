a6c381030ab65e88f34a356112cfba50b8df7360
kon <kokondo@mail.doshisha.ac.jp>
2015-11-22 16:13:44 -0500

    20151122a. Add matlab/@multi/plot.m

---
+++ matlab/TEST_plot_multi.m
@@ -0,0 +1,16 @@
+function TEST_plot_multi
+clear all
+close all
+
+x=multi(0:0.1:10);
+y=sin(x);
+z=sin(x)+0.5*sin(2*x)+0.25*sin(4*x);
+
+S = get(0, 'ScreenSize');
+figure('Position',S);
+plot(x,y,'k-o',x,z,'k-x','LineWidth',3,'MarkerSize',18,'MarkerEdgeColor','k','MarkerFaceColor',[1 1 1]);
+grid on;
+grid minor;
+xlabel('x','FontName','Times','FontSize',24,'FontAngle','italic','FontWeight','bold','Color','k');
+ylabel('y','FontName','Times','FontSize',24,'FontAngle','italic','FontWeight','bold','Color','k');
+title('Waves','FontName','Times','FontSize',24,'FontAngle','normal','FontWeight','bold','Color','k');
