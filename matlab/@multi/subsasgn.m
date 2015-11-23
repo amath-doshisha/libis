5ab85936fee07f52200e77f6a96bfc4760db308a
kon <kokondo@mail.doshisha.ac.jp>
2015-11-23 05:35:44 -0500

    20151123. Add matlab/subsasgn

---
+++ matlab/@multi/subsasgn.m
@@ -0,0 +1,19 @@
+%% x=subsasgn(x,s,y)
+function x=subsasgn(x,s,y)
+cmd='subsasgn';
+switch s(1).type
+    case '()'
+        if isa(x,'multi')
+            if isa(y,'multi')
+                x=multi(cmd,x.data,s,y.data);
+            else
+                x=multi(cmd,x.data,s,multi(y).data);
+            end
+        else
+            x=builtin(cmd,x,s,y);
+        end
+    case '.'
+        x=builtin(cmd,x,s,y);
+    otherwise
+        error('subsasgn_error');
+end
