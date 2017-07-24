83da4d5bd0c7acb8d39dc3ba04b035114bbb6fa1
Koichi Kondo <kokondo@mail.doshisha.ac.jp>
2017-07-25 00:07:31 +0900

    20170724
    Change matlab/{subsref,subsasgn}.m

--- matlab/@multi/subsref.m
+++ matlab/@multi/subsref.m
@@ -1,13 +1,28 @@
 % y=subsref(x,s)
-function y=subsref(x,s)
+function varargout=subsref(x,s)
 cmd='subsref';
 switch s(1).type
     case '()'
-        if isa(x,'multi')
-            y=multi(cmd,x.data,s);
-        else
-            y=multi(cmd,multi(x).data,s);
+        if ~isa(x,'multi')
+            x=multi(x);
         end
+        y=multi();
+        y.data=subsref(x.data,s);
+        varargout{1}=y;   
     otherwise
-        y=builtin(cmd,x,s);
+        varargout={builtin(cmd,x,s)};
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% old version                                        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% cmd='subsref';
+% switch s(1).type
+%     case '()'
+%         if isa(x,'multi')
+%             varargout{1}=multi(cmd,x.data,s);
+%         else
+%             varargout{1}=multi(cmd,multi(x).data,s);
+%         end
+%     otherwise
+%         varargout={builtin(cmd,x,s)};
+% end
--- matlab/sample5.m
+++ matlab/sample5.m
@@ -1,9 +1,37 @@
 %function sample5
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all
 close all
-n=10;
-A=rand(n)
+%n=10;A=rand(n)
+A=toeplitz([1 2 0 1 0],[1 2 0 0 0])
 lambda=sort(eig(A))
-plot(real(lambda),imag(lambda),'o')
+
+set_default_prec(512)
+A=multi(A);
+lambda2=sort(double(eig(A)))
+
+plot(real(lambda),imag(lambda),'o',real(lambda2),imag(lambda2),'x')
 axis square
 grid on
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% clear all
+% close all
+% 
+% set_default_prec(512)
+% A=zeros(10,10,'multi');
+% A(:)=1:100;
+% A
+% A(44)
+% A(7,:)
+% A(:,5)
+% A(1:2:10,2:2:10)
+% i=1;
+% while i<=100
+%     A(i)=-1*A(i)
+%     i=i+1;
+% end
