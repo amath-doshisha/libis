8bdf90e409a404a68de224a362e6d1cae7128788
hiro-matsuura <34119470+hiro-matsuura@users.noreply.github.com>
2017-12-18 18:57:46 +0900

    Delete get_default_round_mode.m

--- get_default_round_mode.m
+++
@@ -1,6 +0,0 @@
-function mode=get_default_round_mode()
-global default_round_mode;
-if exist('default_round_mode','var')==0 || length(default_round_mode)~=1
-    default_round_mode=0;
-end
-mode=default_round_mode;
