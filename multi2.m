603a7477871e844631e25f09dbef91b985d24fd4
hiro-matsuura <34119470+hiro-matsuura@users.noreply.github.com>
2017-12-18 18:59:19 +0900

    Delete multi2.m

--- multi2.m
+++
@@ -1,8 +0,0 @@
-function [out1,out2]=multi2(cmd,varargin)
-if nargout<=1
-    out1=multi_mex(cmd,get_default_prec(),double(get_default_round_mode()),double(get_auto_prec_mode()),varargin{1:end});
-elseif nargout<=2
-    [out1,out2]=multi_mex(cmd,get_default_prec(),double(get_default_round_mode()),double(get_auto_prec_mode()),varargin{1:end});
-else
-    error('multi2: Too many outputs.');
-end
